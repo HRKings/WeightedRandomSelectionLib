cbdec809334b29c30802b712c2d0b9b628af1de9
Helton Reis <heltonkreis@gmail.com>
2021-02-11 22:46:57 -0300

    [refactor] Code cleanup

--- WeightedRandomSelectionLib/Structure/SelectorOptions.cs
+++ WeightedRandomSelectionLib/Structure/SelectorOptions.cs
@@ -5,19 +5,19 @@ namespace WeightedRandomSelectionLib.Structure
 	[Flags]
 	public enum SelectorOptions : short
 	{
-        /// <summary>
-        ///     No special options will be used
-        /// </summary>
-        None = 0,
+		/// <summary>
+		///     No special options will be used
+		/// </summary>
+		None = 0,
 
-        /// <summary>
-        ///     Allow the same item to appear multiple times
-        /// </summary>
-        AllowDuplicates = 1,
+		/// <summary>
+		///     Allow the same item to appear multiple times
+		/// </summary>
+		AllowDuplicates = 1,
 
-        /// <summary>
-        ///     Items with zero weight will not be considered when selecting items
-        /// </summary>
-        IgnoreZeroWeight = 2
+		/// <summary>
+		///     Items with zero weight will not be considered when selecting items
+		/// </summary>
+		IgnoreZeroWeight = 2
 	}
 }
--- WeightedRandomSelectionLib/WeightedRandomSelector.cs
+++ WeightedRandomSelectionLib/WeightedRandomSelector.cs
@@ -7,217 +7,193 @@ using WeightedRandomSelectionLib.Utils;
 
 namespace WeightedRandomSelectionLib
 {
-    /// <summary>
-    ///     Selects items based on their percentage chances
-    /// </summary>
-    /// <typeparam name="T">The type of the items to select</typeparam>
-    public class WeightedRandomSelector<T> : IWeightedBuilder<T>, IWeightedEngine<T>
+	/// <summary>
+	///     Selects items based on their percentage chances
+	/// </summary>
+	/// <typeparam name="T">The type of the items to select</typeparam>
+	public class WeightedRandomSelector<T> : IWeightedBuilder<T>, IWeightedEngine<T>
 	{
-        /// <summary>
-        ///     All weights are multiplied by this factor to get a integer number
-        ///     <para>All numbers after this decimal place will be ignored</para>
-        ///     <para>If this number is set too hight, might occur performance loss</para>
-        /// </summary>
-        public readonly int IntegerFactor;
+		/// <summary>
+		///     All weights are multiplied by this factor to get a integer number
+		///     <para>All numbers after this decimal place will be ignored</para>
+		///     <para>If this number is set too hight, might occur performance loss</para>
+		/// </summary>
+		private readonly int _integerFactor;
 
-		internal readonly List<WeightedItem<T>> Items;
-		public readonly SelectorOptions Options;
-		
+		private readonly List<WeightedItem<T>> _items;
+		private readonly SelectorOptions _options;
+
 		private readonly Random _rng;
 
-        /// <summary>
-        ///     If this variable is set to true, before making any selections, the cumulative weight of the items will be
-        ///     recalculated
-        /// </summary>
-        private bool _forceRecalculation;
+		private int[] _cumulativeWeights;
+		private List<int> _cumulativeWeightsList;
 
-		internal int[] CumulativeWeights;
-		internal List<int> CumulativeWeightsList;
-		internal int TotalCumulativeWeight;
+		/// <summary>
+		///     If this variable is set to true, before making any selections, the cumulative weight of the items will be
+		///     recalculated
+		/// </summary>
+		private bool _forceRecalculation;
 
-        /// <summary>
-        ///     Creates a instance of the selector with the desired options
-        /// </summary>
-        /// <param name="options"><see cref="SelectorOptions" /> flags for the selector</param>
-        /// <param name="decimalPlaces">The maximum allowed decimal places in the items weights</param>
-        public WeightedRandomSelector(
+		private int _totalCumulativeWeight;
+
+		/// <summary>
+		///     Creates a instance of the selector with the desired options
+		/// </summary>
+		/// <param name="options"><see cref="SelectorOptions" /> flags for the selector</param>
+		/// <param name="decimalPlaces">The maximum allowed decimal places in the items weights</param>
+		public WeightedRandomSelector(
 			SelectorOptions options = SelectorOptions.AllowDuplicates | SelectorOptions.IgnoreZeroWeight,
 			int decimalPlaces = 2)
 		{
-			Items = new List<WeightedItem<T>>();
-			Options = options;
+			_items = new List<WeightedItem<T>>();
+			_options = options;
 
-			CumulativeWeights = null;
-			TotalCumulativeWeight = 0;
-			CumulativeWeightsList = null;
+			_cumulativeWeights = null;
+			_totalCumulativeWeight = 0;
+			_cumulativeWeightsList = null;
 
-			IntegerFactor = (int) Math.Pow(10, decimalPlaces);
-			
+			_integerFactor = (int) Math.Pow(10, decimalPlaces);
+
 			_rng = new Random();
 
 			_forceRecalculation = false;
 		}
 
-        /// <summary>
-        ///     Creates a instance of the selector with an already made list of <see cref="WeightedItem{T}" /> and the desired
-        ///     options
-        ///     <seealso cref="SelectorOptions" />
-        /// </summary>
-        /// <param name="items">A list of <see cref="WeightedItem{T}" /></param>
-        /// <param name="options"><see cref="SelectorOptions" /> flags for the selector</param>
-        /// <param name="decimalPlaces">The maximum allowed decimal places in the items weights</param>
-        public WeightedRandomSelector(List<WeightedItem<T>> items,
+		/// <summary>
+		///     Creates a instance of the selector with an already made list of <see cref="WeightedItem{T}" /> and the desired
+		///     options
+		///     <seealso cref="SelectorOptions" />
+		/// </summary>
+		/// <param name="items">A list of <see cref="WeightedItem{T}" /></param>
+		/// <param name="options"><see cref="SelectorOptions" /> flags for the selector</param>
+		/// <param name="decimalPlaces">The maximum allowed decimal places in the items weights</param>
+		public WeightedRandomSelector(List<WeightedItem<T>> items,
 			SelectorOptions options = SelectorOptions.AllowDuplicates | SelectorOptions.IgnoreZeroWeight,
-			int decimalPlaces = 2)
+			int decimalPlaces = 2) : this(options, decimalPlaces)
 		{
-			Items = items;
-			Options = options;
-
-			CumulativeWeights = null;
-			TotalCumulativeWeight = 0;
-			CumulativeWeightsList = null;
-
-			IntegerFactor = (int) Math.Pow(10, decimalPlaces);
-			
-			_rng = new Random();
-
-			_forceRecalculation = true;
+			_items = items;
 		}
-        
-        /// <summary>
-        ///     Adds a new <see cref="WeightedItem{T}" /> to the selector
-        /// </summary>
-        /// <param name="item">The item to add</param>
-        public void Add(WeightedItem<T> item)
+
+		/// <summary>
+		///     Adds a new <see cref="WeightedItem{T}" /> to the selector
+		/// </summary>
+		/// <param name="item">The item to add</param>
+		public void Add(WeightedItem<T> item)
 		{
 			if (item.Weight <= 0)
 			{
-				if (Options.HasFlag(SelectorOptions.IgnoreZeroWeight))
+				if (_options.HasFlag(SelectorOptions.IgnoreZeroWeight))
 					return;
 				throw new InvalidOperationException("Scores must be >= 0");
 			}
 
 			_forceRecalculation = true;
-			Items.Add(item);
+			_items.Add(item);
 		}
 
-        /// <summary>
-        ///     Adds multiple <see cref="WeightedItem{T}" /> to the selector
-        /// </summary>
-        /// <param name="items">A Enumerable collection on <see cref="WeightedItem{T}" /></param>
-        public void Add(IEnumerable<WeightedItem<T>> items)
+		/// <summary>
+		///     Adds multiple <see cref="WeightedItem{T}" /> to the selector
+		/// </summary>
+		/// <param name="items">A Enumerable collection on <see cref="WeightedItem{T}" /></param>
+		public void Add(IEnumerable<WeightedItem<T>> items)
 		{
 			foreach (var i in items) Add(i);
 		}
 
-        /// <summary>
-        ///     Adds a new <see cref="WeightedItem{T}" /> with the desired value and weight
-        /// </summary>
-        /// <param name="value">The value of the item</param>
-        /// <param name="weight">The weight of the item (up to two decimal places)</param>
-        public void Add(T value, double weight)
+		/// <summary>
+		///     Adds a new <see cref="WeightedItem{T}" /> with the desired value and weight
+		/// </summary>
+		/// <param name="value">The value of the item</param>
+		/// <param name="weight">The weight of the item (up to two decimal places)</param>
+		public void Add(T value, double weight)
 		{
 			Add(new WeightedItem<T>(value, weight));
 		}
 
-        /// <summary>
-        ///     Removes the specified <see cref="WeightedItem{T}" /> from the selector
-        /// </summary>
-        /// <param name="item">The item to remove</param>
-        public void Remove(WeightedItem<T> item)
+		/// <summary>
+		///     Removes the specified <see cref="WeightedItem{T}" /> from the selector
+		/// </summary>
+		/// <param name="item">The item to remove</param>
+		public void Remove(WeightedItem<T> item)
 		{
 			_forceRecalculation = true;
-			Items.Remove(item);
+			_items.Remove(item);
 		}
 
-        /// <summary>
-        ///     Clears the items list
-        /// </summary>
-        public void Clear()
+		/// <summary>
+		///     Clears the items list
+		/// </summary>
+		public void Clear()
 		{
 			_forceRecalculation = true;
-			Items.Clear();
+			_items.Clear();
 		}
-        
-        /// <summary>
-        ///     Calculates the cumulative weights if it's needed
-        /// </summary>
-        public void Build()
+
+		/// <summary>
+		///     Calculates the cumulative weights if it's needed
+		/// </summary>
+		public void Build()
 		{
 			if (!_forceRecalculation)
 				return;
 
 			_forceRecalculation = false;
-			(CumulativeWeights, TotalCumulativeWeight) = WeightedHelper<T>.CalculateCumulativeWeights(Items, IntegerFactor);
+			(_cumulativeWeights, _totalCumulativeWeight) =
+				WeightedHelper<T>.CalculateCumulativeWeights(_items, _integerFactor);
 
 			// If the selector allow duplicates then it don't have to create a copy of the weights in list form
-			if (!Options.HasFlag(SelectorOptions.AllowDuplicates))
-				CumulativeWeightsList = CumulativeWeights.ToList();
+			if (!_options.HasFlag(SelectorOptions.AllowDuplicates))
+				_cumulativeWeightsList = _cumulativeWeights.ToList();
 		}
-        
-        /// <summary>
-        ///     A method to generate a number for the selector
-        /// </summary>
-        /// <param name="items">A list containing all the <see cref="WeightedItem{T}" /></param>
-        /// <returns>A number between 1 and the sum of all the weights of items in the list</returns>
-        private int RollRNG(IEnumerable<WeightedItem<T>> items)
-		{
-			// If the selector allow duplicates, there is no need to recalculate the total cumulative weight every time
-			int maxWeight = Options.HasFlag(SelectorOptions.AllowDuplicates)
-				? TotalCumulativeWeight + 1
-				: (int) items.Sum(i => i.Weight * IntegerFactor) + 1;
 
-			return _rng.Next(1, maxWeight);
-		}
-        
-        /// <summary>
-        ///     Selects a single item based in the weights
-        /// </summary>
-        /// <returns>The value of the item</returns>
-        public T Select()
+		/// <summary>
+		///     Selects a single item based in the weights
+		/// </summary>
+		/// <returns>The value of the item</returns>
+		public T Select()
 		{
 			Build();
-			
-			var items = Items;
+
+			var items = _items;
 
 			if (items.Count == 0)
 				throw new InvalidOperationException("There was no items to select from");
 
-			return WeightedHelper<T>.SelectItem(items, CumulativeWeights, RollRNG(items)).Value;
+			return WeightedHelper<T>.SelectItem(items, _cumulativeWeights, GenerateRandomWeight(items)).Value;
 		}
 
-        /// <summary>
-        ///     Selects multiple items based in the weights
-        /// </summary>
-        /// <param name="count">The number of items to select</param>
-        /// <returns>A list containing the values of the items selected</returns>
-        public IEnumerable<T> Select(int count)
+		/// <summary>
+		///     Selects multiple items based in the weights
+		/// </summary>
+		/// <param name="count">The number of items to select</param>
+		/// <returns>A list containing the values of the items selected</returns>
+		public IEnumerable<T> Select(int count)
 		{
 			if (count <= 0)
 				throw new InvalidOperationException("Count must be > 0.");
 
-			int itemsCount = Items.Count;
+			int itemsCount = _items.Count;
 
 			if (itemsCount == 0)
 				throw new InvalidOperationException("There were no items to select from.");
 
-			if (!Options.HasFlag(SelectorOptions.AllowDuplicates) && itemsCount < count)
+			if (!_options.HasFlag(SelectorOptions.AllowDuplicates) && itemsCount < count)
 				throw new InvalidOperationException("There aren't enough items in the collection to take " + count);
-			
+
 			Build();
 
 			var resultList = new List<T>();
 
 			// A implementation of the select multiple code that uses O(log(n)) even when removing items from the list
-			if (!Options.HasFlag(SelectorOptions.AllowDuplicates))
+			if (!_options.HasFlag(SelectorOptions.AllowDuplicates))
 			{
 				// A shallow copy of the lists containing the items and the cumulative weights, since we will be removing items of the list we want to conserve the original ones
-				var items = new List<WeightedItem<T>>(Items);
-				var weights = new List<int>(CumulativeWeightsList);
+				var items = new List<WeightedItem<T>>(_items);
+				var weights = new List<int>(_cumulativeWeightsList);
 
 				for (var i = 0; i < count; i++)
 				{
-					int index = WeightedHelper<T>.SelectItemIndex(items, weights, RollRNG(items));
+					int index = WeightedHelper<T>.SelectItemIndex(items, weights, GenerateRandomWeight(items));
 					resultList.Add(items[index].Value);
 
 					if (items.Count > 0)
@@ -229,10 +205,26 @@ namespace WeightedRandomSelectionLib
 			}
 			else
 			{
-				for (var i = 0; i < count; i++) resultList.Add(WeightedHelper<T>.SelectItem(Items, CumulativeWeights, RollRNG(Items)).Value);
+				for (var i = 0; i < count; i++)
+					resultList.Add(WeightedHelper<T>.SelectItem(_items, _cumulativeWeights, GenerateRandomWeight(_items)).Value);
 			}
 
 			return resultList;
+		}
+
+		/// <summary>
+		///     A method to generate a number for the selector
+		/// </summary>
+		/// <param name="items">A list containing all the <see cref="WeightedItem{T}" /></param>
+		/// <returns>A number between 1 and the sum of all the weights of items in the list</returns>
+		private int GenerateRandomWeight(IEnumerable<WeightedItem<T>> items)
+		{
+			// If the selector allow duplicates, there is no need to recalculate the total cumulative weight every time
+			int maxWeight = _options.HasFlag(SelectorOptions.AllowDuplicates)
+				? _totalCumulativeWeight + 1
+				: (int) items.Sum(i => i.Weight * _integerFactor) + 1;
+
+			return _rng.Next(1, maxWeight);
 		}
 	}
 }
