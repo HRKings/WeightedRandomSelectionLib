4996c68b8310b7bbfebc67fbc0dc76bfc12cba82
Helton Reis <hrkings@rhaizzing.com>
2021-02-11 22:08:18 -0300

    [feature] Add cumulative weight calculator helper

---
+++ WeightedRandomSelectionLib/Utils/WeightedHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WeightedRandomSelectionLib.Structure;
+
+namespace WeightedRandomSelectionLib.Utils
+{
+	public static class WeightedHelper<T>
+	{
+		public static (int[], int) CalculateCumulativeWeights(List<WeightedItem<T>> weights, int integerFactor)
+		{
+			var currentWeight = 0;
+			var index = 0;
+			var result = new int[weights.Count];
+			var totalCumulativeWeight = 0;
+
+			foreach (int roundedWeight in weights.Select(weight => (int) (weight.Weight * integerFactor)))
+			{
+				currentWeight += roundedWeight;
+				totalCumulativeWeight += roundedWeight;
+
+				result[index] = currentWeight;
+
+				index++;
+			}
+
+			return (result, totalCumulativeWeight);
+		}
+	}
+}
