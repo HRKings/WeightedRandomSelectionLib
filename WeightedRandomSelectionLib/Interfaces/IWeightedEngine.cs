6cb297b57f95274b37dd8395a3fcc06eb6ec6eb4
Helton Reis <hrkings@rhaizzing.com>
2021-02-11 22:02:36 -0300

    [feature] Add engine interface

---
+++ WeightedRandomSelectionLib/Interfaces/IWeightedEngine.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WeightedRandomSelectionLib.Interfaces
+{
+	public interface IWeightedEngine<T>
+	{
+		public T Select();
+		public IEnumerable<T> Select(int amount);
+	}
+}
