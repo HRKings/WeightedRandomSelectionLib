2611e76920cf468d7b4621455d763b090d1e0194
Helton Reis <heltonkreis@gmail.com>
2021-02-11 22:16:06 -0300

    [feature] Add more methods to the builder interface

--- WeightedRandomSelectionLib/Interfaces/IWeightedBuilder.cs
+++ WeightedRandomSelectionLib/Interfaces/IWeightedBuilder.cs
@@ -1,7 +1,15 @@
-﻿namespace WeightedRandomSelectionLib.Interfaces
+﻿using System.Collections.Generic;
+using WeightedRandomSelectionLib.Structure;
+
+namespace WeightedRandomSelectionLib.Interfaces
 {
-	public interface IWeightedBuilder
+	public interface IWeightedBuilder<T>
 	{
 		public void Build();
+		public void Add(WeightedItem<T> item);
+		public void Add(T item, double weight);
+		public void Add(IEnumerable<WeightedItem<T>> item);
+		public void Remove(WeightedItem<T> item);
+		public void Clear();
 	}
 }
